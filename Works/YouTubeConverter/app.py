0b3d359d3ea3175a0b03a90045d53e097814355a
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-10-09 03:03:22 +0400

    YouTube Converter & Downloader

--- Works/YouTubeConverter/app.py
+++ Works/YouTubeConverter/app.py
@@ -2,18 +2,43 @@ from flask import Flask,redirect,url_for,render_template,request,send_file,sessi
 from pytube import YouTube
 from tempfile import TemporaryDirectory
 from io import BytesIO
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SECRET_KEY'] = "MySecret"
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///example.db'
+
+
+ENV = 'prod'
+
+if ENV == 'dev':
+    app.debug = True
+    # app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:yusif@localhost:5432/url'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI']='postgresql://hqsmehndaqglmb:2319825064d8b3162275e8530588558884037bc872f8530a74e7b65b7620ccd3@ec2-23-23-162-138.compute-1.amazonaws.com:5432/d8fsij8tfddd3a'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db=SQLAlchemy(app)
+
+class Urls(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    link = db.Column(db.String(250))
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    
+    links = Urls.query.all()
     if request.method=='POST':
         session['link'] = request.form.get('url')
         url = YouTube(session['link'])
+        urls = Urls(
+            link = request.form['url']
+        )
+        db.session.add(urls)
+        db.session.commit()
         return render_template('video.html', url=url)
-    return render_template('index.html')
+    return render_template('index.html', links=links)
 
 @app.route("/download", methods=["GET","POST"])
 def download_video():
