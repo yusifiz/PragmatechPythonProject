2fe650df737961aa12cc9e389a2cb6f2c8a7aea4
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-10-18 16:45:43 +0400

    Weekend-04 Django tasks

--- Algoritms/PythonFundamentals-Week03/blog/models.py
+++ Algoritms/PythonFundamentals-Week03/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=127, null=True, blank=True)
+    text = models.TextField(help_text='Main info..')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+        
