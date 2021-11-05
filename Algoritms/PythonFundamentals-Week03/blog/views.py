faa99460cc37f7fca394ce7815ccf8aaba8ea669
Yusif <86804534+yusifiz@users.noreply.github.com>
2021-11-05 16:52:29 +0400

    Delete Algoritms directory

--- Algoritms/PythonFundamentals-Week03/blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Blog
-# Create your views here.
-def blog(request):
-    blogs = Blog.objects.all()
-
-    return render(request,'blog.html',{'blogs' : blogs})
-
