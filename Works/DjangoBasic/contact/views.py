33641a4264aad22ab2d35bdc09d045be9cb24430
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-11-10 18:50:32 +0400

    django form flash messages

--- Works/DjangoBasic/contact/views.py
+++ Works/DjangoBasic/contact/views.py
@@ -2,7 +2,7 @@ from django import forms
 from django.shortcuts import redirect, render
 from . forms import ContactForm
 from contact.utils.validators import ValidationError
-
+from django.contrib import messages
 
 
 def contact(request):
@@ -11,6 +11,9 @@ def contact(request):
         form = ContactForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/')
+            messages.add_message(request, messages.INFO, 'Successfuly registration!')
+            return redirect('/contact')
+        else:
+            messages.add_message(request, messages.INFO, 'Your mail is not valid!')
         
     return render(request, 'contact.html', {'form': form})
