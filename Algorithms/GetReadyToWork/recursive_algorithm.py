14c302bc81937e5070de4dfac953864851997d32
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-01-28 01:24:45 +0400

    Recursive Algorithm: Factorial example

---
+++ Algorithms/GetReadyToWork/recursive_algorithm.py
@@ -0,0 +1,11 @@
+# Recursive Algorithm
+
+def factorial(x):
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
