4d44eea65f67237f32d62220e6132c9371766a69
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-01-28 02:04:13 +0400

    Divide and Conquer Algorithms: Merge Sort Algorithm

--- Algorithms/GetReadyToWork/divide_conquer_algorithms.py
+++ Algorithms/GetReadyToWork/divide_conquer_algorithms.py
@@ -52,3 +52,63 @@ def divideAndConquer_Min(arr, ind, len):
 minimum = divideAndConquer_Min(arr, 0, 9)
 
 print("The minimum number in the array is: ", minimum)
+
+
+## MergeSort Algorithm
+
+def mergeSort(arr):
+	if len(arr) > 1:
+
+		# Arrayin ortasini tapiriq
+		mid = len(arr)//2
+
+		# Array elementlerini boluruk
+		L = arr[:mid]
+
+		R = arr[mid:]
+
+		# Birinci hisseni sort edirik
+		mergeSort(L)
+
+		# Ikinci hisseni sort edirik
+		mergeSort(R)
+
+		i = j = k = 0
+
+		while i < len(L) and j < len(R):
+			if L[i] < R[j]:
+				arr[k] = L[i]
+				i += 1
+			else:
+				arr[k] = R[j]
+				j += 1
+			k += 1
+
+
+		while i < len(L):
+			arr[k] = L[i]
+			i += 1
+			k += 1
+
+		while j < len(R):
+			arr[k] = R[j]
+			j += 1
+			k += 1
+
+
+
+def printList(arr):
+	for i in range(len(arr)):
+		print(arr[i], end=" ")
+	print()
+
+
+if __name__ == '__main__':
+	arr = [12, 11, 13, 5, 6, 7]
+	print("Given array is", end="\n")
+	printList(arr)
+	mergeSort(arr)
+	print("Sorted array is: ", end="\n")
+	printList(arr)
+
+
