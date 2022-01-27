9460bf00d220fbfeebe8cc649f5efd096565fd2d
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-01-28 00:58:54 +0400

    Brute Force Algorithm: Bubble Sort Algorithm

--- Algorithms/GetReadyToWork/brute_force_algorithms.py
+++ Algorithms/GetReadyToWork/brute_force_algorithms.py
@@ -12,3 +12,22 @@ def linear_search(haystack, needle):
 print(linear_search([4, 5, 2, 7, 1, 8], 8))
 
 
+# Bubble Sort Algorithm
+
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+
+        for j in range(0, n-i-1):
+
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j] # eger element novbeti elementden boyukdurse, yerlerini deyis
+
+        print(arr)
+
+
+arr = [3, 8, 2, 5, 6, 1, 9, 4, 7]
+bubble_sort(arr)
+print("Sorted array is:", arr)
