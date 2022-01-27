a6a3b2b8119eb2377e0d7b332841aa5318b767f9
Yusif Osmanov <yusifosmanov475@gmail.com>
2022-01-28 01:22:01 +0400

    Brute Force Algorithm: Bubble Sort

--- Algorithms/GetReadyToWork/brute_force_algorithms.py
+++ Algorithms/GetReadyToWork/brute_force_algorithms.py
@@ -31,3 +31,30 @@ def bubble_sort(arr):
 arr = [3, 8, 2, 5, 6, 1, 9, 4, 7]
 bubble_sort(arr)
 print("Sorted array is:", arr)
+
+
+def bubbleSort(array):
+    
+  for i in range(len(array)):
+
+    swapped = False
+
+    for j in range(0, len(array) - i - 1):
+
+      if array[j] > array[j + 1]:
+
+        temp = array[j]
+        array[j] = array[j+1]
+        array[j+1] = temp
+
+        swapped = True
+          
+    if not swapped:
+      break
+
+data = [-2, 45, 0, 11, -9]
+
+bubbleSort(data)
+
+print('Sorted Array:')
+print(data)
